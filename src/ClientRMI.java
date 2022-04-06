ce19d7567f4c9c3fb4df16cd180233c10349a11e
cosmos2028 <lestat290@hotmail.fr>
2022-04-06 14:35:28 +0200

    CLIENT RMI GestBanque_Spring_Boot_JPA_Hibernate_Restful_SOAP_RMI_Angular

---
+++ src/ClientRMI.java
@@ -0,0 +1,27 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.glsid.entities.Compte;
+import org.glsid.serviceRMI.BanqueRmiRemote;
+
+public class ClientRMI {
+
+	public static void main(String[] args) 
+	{
+
+		try 
+		{
+			BanqueRmiRemote stub = (BanqueRmiRemote) Naming.lookup("rmi://localhost:1099/BK");
+			Compte cpte = stub.getCompte("CC3");
+			System.out.println(cpte.getSolde());
+			System.out.println(cpte.getIdCompte());
+			System.out.println(cpte.getDateCreation());
+			
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		} 
+	}
+
+}
